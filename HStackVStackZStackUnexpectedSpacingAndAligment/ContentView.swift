6d4901ad8a5a26021c70499109c03423b3f624bf
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2019-10-04 13:09:12 +0300

    init

--- HStackVStackZStackUnexpectedSpacingAndAligment/ContentView.swift
+++ HStackVStackZStackUnexpectedSpacingAndAligment/ContentView.swift
@@ -10,7 +10,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello World")
+        VStack {
+            Text("123").background(Color.red)
+            Spacer().frame(width: 10).background(Color.orange)
+            Text("Botton")
+        }
     }
 }
 
